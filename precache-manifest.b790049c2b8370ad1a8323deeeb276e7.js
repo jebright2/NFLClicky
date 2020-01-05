4568c08dbb2d94d207992329dc29ba8a494a4a12
Da Vinci <jeb2je@gmail.com>
2020-01-04 20:01:29 -0500

    Updates

---
+++ precache-manifest.b790049c2b8370ad1a8323deeeb276e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363ea88af3b531119e113dc77f23bdcb",
+    "url": "/NFLClicky/index.html"
+  },
+  {
+    "revision": "4a2d778e384afa07d9d0",
+    "url": "/NFLClicky/static/css/main.ae97c07d.chunk.css"
+  },
+  {
+    "revision": "74a75abb588cbcf9b8b6",
+    "url": "/NFLClicky/static/js/2.b79bc36e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/NFLClicky/static/js/2.b79bc36e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a2d778e384afa07d9d0",
+    "url": "/NFLClicky/static/js/main.e31ae290.chunk.js"
+  },
+  {
+    "revision": "70c0c098e824f4be9435",
+    "url": "/NFLClicky/static/js/runtime-main.d3481ba6.js"
+  }
+]);
